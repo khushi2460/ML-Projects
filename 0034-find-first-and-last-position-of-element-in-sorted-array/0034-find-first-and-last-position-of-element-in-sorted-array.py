6bef0280ee19961f6613da7230d5b47f59081845
khushi2460 <khushi4602@gmail.com>
2026-05-12 14:35:11 +0530

    Time: 0 ms (100%), Space: 20.6 MB (59.6%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,35 +1,33 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def lb():
-            l,r=0,len(nums)-1
-            lb=-1
-            while l<=r:
-                mid=(l+r)//2
-                if nums[mid]==target:
-                    lb=mid
-                    r=mid-1
-                elif nums[mid]>target:
-                    r=mid-1
+
+        def findFirst():
+            l, r, ans = 0, len(nums) - 1, -1
+            while l <= r:
+                mid = (l + r) // 2
+                if nums[mid] == target:
+                    ans = mid
+                    r = mid - 1  
+                elif nums[mid] < target:
+                    l = mid + 1
                 else:
-                    l=mid+1
-            return lb
-        def up():
-            l,r=0,len(nums)-1
-            ub=-1
-            while l<=r:
-                mid=(l+r)//2
-                if nums[mid]<=target:
-                    if nums[mid]==target:
-                        ub=mid
-                    l=mid+1
+                    r = mid - 1
+            return ans
+
+        def findLast():
+            l, r, ans = 0, len(nums) - 1, -1
+            while l <= r:
+                mid = (l + r) // 2
+                if nums[mid] == target:
+                    ans = mid
+                    l = mid + 1  
+                elif nums[mid] < target:
+                    l = mid + 1
                 else:
-                    r=mid-1
-            return ub
+                    r = mid - 1
+            return ans
+
+        return [findFirst(), findLast()]
+
+
         
-        return [lb(),up()]                    
-
-             
-                         
-
-
-            
