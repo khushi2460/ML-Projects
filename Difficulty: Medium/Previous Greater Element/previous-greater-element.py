68168dbafc44e7d9e7de2783d8a2ef3b9ead8f25
khushi2460 <khushi4602@gmail.com>
2026-05-14 16:47:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Previous Greater Element/previous-greater-element.py
@@ -0,0 +1,14 @@
+class Solution:
+    def preGreaterEle(self, arr):
+        n = len(arr)
+        ans = [-1] * n
+        stack = []  
+        for i in range(n):
+            while stack and stack[-1] <= arr[i]:
+                stack.pop()
+            if stack:
+                ans[i] = stack[-1]
+            stack.append(arr[i])
+        
+        return ans
+
